ab83ef5ab0ec5e5cdcdf5d20a1b5da1a7bf04c42
livinglist <georgefung78@live.com>
2020-05-13 03:16:46 -0700

    Fix weird issues.

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/george/Development/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/george/Development/Kanji_Dictionary/kanji_dictionary"
-export "FLUTTER_TARGET=/Users/george/Development/Kanji_Dictionary/kanji_dictionary/lib/main.dart"
+export "FLUTTER_APPLICATION_PATH=/Users/george/Development/manji/Manji"
+export "FLUTTER_TARGET=/Users/george/Development/manji/Manji/lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
 export "FLUTTER_FRAMEWORK_DIR=/Users/george/Development/flutter/bin/cache/artifacts/engine/ios"
