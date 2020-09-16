6ad6a100855a73530b725442c6994268f50e9338
livinglist <georgefung78@live.com>
2020-09-15 20:04:56 -0700

    Fixed the bug where sometimes non-kanji characters will be extracted from the image.

--- lib/ui/text_recognize_page/text_recognize_page.dart
+++ lib/ui/text_recognize_page/text_recognize_page.dart
@@ -106,125 +106,109 @@ class _TextRecognizePageState extends State<TextRecognizePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        actions: <Widget>[
-          didChooseImage
-              ? StreamBuilder(
-                  stream: textRecognizeBloc.text,
-                  builder: (_, AsyncSnapshot<String> snapshot) {
-                    if (snapshot.hasData) {
-                      text = snapshot.data;
-                      if (text.isEmpty) {
-                        return Container();
-                      }
+    return StreamBuilder(
+      stream: textRecognizeBloc.text,
+      builder: (_, AsyncSnapshot<String> snapshot) {
+        text = snapshot.data;
 
-                      kanjis = text.getKanjis().map((str) => KanjiBloc.instance.allKanjisMap[str]).toList();
+        if (text != null && text.isNotEmpty) {
+          kanjis = text
+              .getKanjis()
+              .where((str) => KanjiBloc.instance.allKanjisMap.containsKey(str))
+              .map((str) => KanjiBloc.instance.allKanjisMap[str])
+              .toList();
+        }
 
-                      if (kanjis.isEmpty) {
-                        return Container();
-                      }
+        Widget body;
 
-                      return IconButton(
-                        icon: AnimatedCrossFade(
-                          firstChild: Icon(
-                            Icons.view_headline,
-                            color: Colors.white,
-                          ),
-                          secondChild: Icon(
-                            Icons.view_comfy,
-                            color: Colors.white,
-                          ),
-                          crossFadeState: showGrid ? CrossFadeState.showFirst : CrossFadeState.showSecond,
-                          duration: Duration(milliseconds: 200),
-                        ),
-                        onPressed: () {
-                          setState(() {
-                            showGrid = !showGrid;
-                          });
-                        },
-                      );
-                    } else {
-                      return Container();
-                    }
-                  },
-                )
-              : Container()
-        ],
-        title: FuriganaText(
-          text: '画像漢字認識',
-          tokens: [
-            Token(text: '画像', furigana: 'がぞう'),
-            Token(text: '漢字', furigana: 'かんじ'),
-            Token(text: '認識', furigana: 'にんしき'),
-          ],
-          style: TextStyle(fontSize: 18),
-        ),
-        elevation: showShadow ? 8 : 0,
-      ),
-      backgroundColor: Theme.of(context).primaryColor,
-      body: didChooseImage
-          ? StreamBuilder(
-              stream: textRecognizeBloc.text,
-              builder: (_, AsyncSnapshot<String> snapshot) {
-                if (snapshot.hasData) {
-                  text = snapshot.data;
-                  if (text.isEmpty) {
-                    return Center(child: Text("No kanji was found in the image.", style: TextStyle(color: Colors.white70)));
-                  }
-
-                  kanjis = text.getKanjis().map((str) => KanjiBloc.instance.allKanjisMap[str]).toList();
-
-                  if (kanjis.isEmpty) {
-                    return Center(child: Text("No kanji was found in the image.", style: TextStyle(color: Colors.white70)));
-                  }
-
-                  return SingleChildScrollView(
-                    controller: scrollController,
-                    child: Flex(
-                      direction: Axis.vertical,
-                      children: <Widget>[
-                        Text(
-                          text,
-                          style: TextStyle(color: Colors.white),
-                        ),
-                        Container(
-                          width: MediaQuery.of(context).size.width,
-                          child: Padding(
-                            padding: EdgeInsets.only(top: 8, bottom: 8, left: 20),
-                            child: Text(
-                              "Found ${kanjis.length} kanji in the image:",
-                              style: TextStyle(color: Colors.white, fontSize: 16),
-                            ),
-                          ),
-                        ),
-                        showGrid
-                            ? KanjiGridView(kanjis: kanjis, scrollPhysics: NeverScrollableScrollPhysics())
-                            : KanjiListView(kanjis: kanjis, scrollPhysics: NeverScrollableScrollPhysics())
-                      ],
-                    ),
-                  );
-                } else {
-                  return Center(child: CircularProgressIndicator());
-                }
-              },
-            )
-          : Center(
+        if (!didChooseImage) {
+          body = Center(
               child: Text(
-              "Choose an image first.",
-              style: TextStyle(color: Colors.white70),
-            )),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () => getImageSource().then((val) {
-          if (val != null) {
-            imageSource = val;
-            getImage();
-          }
-        }),
-        tooltip: 'Pick Image',
-        child: Icon(Icons.add_a_photo),
-      ),
+            "Choose an image first.",
+            style: TextStyle(color: Colors.white70),
+          ));
+        } else if (!snapshot.hasData) {
+          body = Center(child: CircularProgressIndicator());
+        } else if (text.isEmpty || kanjis.isEmpty) {
+          body = Center(child: Text("No kanji was found in the image.", style: TextStyle(color: Colors.white70)));
+        } else {
+          body = SingleChildScrollView(
+            controller: scrollController,
+            child: Flex(
+              direction: Axis.vertical,
+              children: <Widget>[
+                Text(
+                  text,
+                  style: TextStyle(color: Colors.white),
+                ),
+                Container(
+                  width: MediaQuery.of(context).size.width,
+                  child: Padding(
+                    padding: EdgeInsets.only(top: 8, bottom: 8, left: 20),
+                    child: Text(
+                      "Found ${kanjis.length} kanji in the image:",
+                      style: TextStyle(color: Colors.white, fontSize: 16),
+                    ),
+                  ),
+                ),
+                showGrid
+                    ? KanjiGridView(kanjis: kanjis, scrollPhysics: NeverScrollableScrollPhysics())
+                    : KanjiListView(kanjis: kanjis, scrollPhysics: NeverScrollableScrollPhysics())
+              ],
+            ),
+          );
+        }
+
+        return Scaffold(
+          appBar: AppBar(
+            centerTitle: true,
+            actions: <Widget>[
+              if (didChooseImage && snapshot.hasData)
+                IconButton(
+                  icon: AnimatedCrossFade(
+                    firstChild: Icon(
+                      Icons.view_headline,
+                      color: Colors.white,
+                    ),
+                    secondChild: Icon(
+                      Icons.view_comfy,
+                      color: Colors.white,
+                    ),
+                    crossFadeState: showGrid ? CrossFadeState.showFirst : CrossFadeState.showSecond,
+                    duration: Duration(milliseconds: 200),
+                  ),
+                  onPressed: () {
+                    setState(() {
+                      showGrid = !showGrid;
+                    });
+                  },
+                ),
+            ],
+            title: FuriganaText(
+              text: '画像漢字認識',
+              tokens: [
+                Token(text: '画像', furigana: 'がぞう'),
+                Token(text: '漢字', furigana: 'かんじ'),
+                Token(text: '認識', furigana: 'にんしき'),
+              ],
+              style: TextStyle(fontSize: 18),
+            ),
+            elevation: showShadow ? 8 : 0,
+          ),
+          backgroundColor: Theme.of(context).primaryColor,
+          body: body,
+          floatingActionButton: FloatingActionButton(
+            onPressed: () => getImageSource().then((val) {
+              if (val != null) {
+                imageSource = val;
+                getImage();
+              }
+            }),
+            tooltip: 'Pick Image',
+            child: Icon(Icons.add_a_photo),
+          ),
+        );
+      },
     );
   }
 }
