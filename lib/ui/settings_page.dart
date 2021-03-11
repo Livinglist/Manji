f6f893c493183f2de8c5a993a5af6bdb47572926
Livinglist <georgefung78@live.com>
2021-03-11 11:59:11 -0800

    Changed version number

--- lib/ui/settings_page.dart
+++ lib/ui/settings_page.dart
@@ -186,7 +186,7 @@ class _SettingsPageState extends State<SettingsPage> {
                   ),
                 ),
                 applicationName: "Manji",
-                applicationVersion: "v2.4.6",
+                applicationVersion: "v2.4.7",
                 aboutBoxChildren: <Widget>[
                   ElevatedButton(
                     onPressed: () {
