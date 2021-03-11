85f2f91068ceb3d5ded002199f3b84fbd7045fe3
Livinglist <georgefung78@live.com>
2021-03-11 12:37:14 -0800

    Fixes Android platform issues.

--- android/app/src/main/java/jiaqifeng/kanji_dictionary/MainActivity.java
+++ android/app/src/main/java/jiaqifeng/kanji_dictionary/MainActivity.java
@@ -1,13 +1,7 @@
 package jiaqifeng.kanji_dictionary;
 
-import android.os.Bundle;
-import io.flutter.app.FlutterActivity;
-import io.flutter.plugins.GeneratedPluginRegistrant;
+import io.flutter.embedding.android.FlutterActivity;
 
 public class MainActivity extends FlutterActivity {
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    GeneratedPluginRegistrant.registerWith(this);
-  }
+
 }
