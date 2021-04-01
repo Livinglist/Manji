7dd2fd2d5b2c7eb1e8faea5e8731a65b2068df5e
Livinglist <georgefung78@live.com>
2021-04-01 04:24:32 -0700

    Removed unused packages.

--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -4,7 +4,6 @@ import 'dart:math';
 import 'package:feature_discovery/feature_discovery.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
-import 'package:firebase_ml_vision/firebase_ml_vision.dart';
 import 'package:flutter/scheduler.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:image_picker/image_picker.dart' show ImageSource;
@@ -39,7 +38,6 @@ class HomePage extends StatefulWidget {
 }
 
 class HomePageState extends State<HomePage> with SingleTickerProviderStateMixin {
-  final TextRecognizer textRecognizer = FirebaseVision.instance.textRecognizer();
   final textEditingController = TextEditingController();
   final scaffoldKey = GlobalKey<ScaffoldState>();
   final backgroundKey = GlobalKey<HomePageBackgroundState>();
