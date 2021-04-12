25b8950baf0f67a2e1d0ac6d4c70dd936cdb6574
Livinglist <georgefung78@live.com>
2021-04-11 23:26:22 -0700

    Removed tests.

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -12,27 +12,4 @@ import 'package:kanji_dictionary/main.dart';
 import 'package:kanji_dictionary/models/kanji.dart';
 import 'package:kanji_dictionary/ui/kanji_detail_page/kanji_detail_page.dart';
 
-void main() {
-  // testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-  //   // Build our app and trigger a frame.
-  //   await tester.pumpWidget(MyApp());
-  //
-  //   // Verify that our counter starts at 0.
-  //   expect(find.text('0'), findsOneWidget);
-  //   expect(find.text('1'), findsNothing);
-  //
-  //   // Tap the '+' icon and trigger a frame.
-  //   await tester.tap(find.byIcon(Icons.add));
-  //   await tester.pump();
-  //
-  //   // Verify that our counter has incremented.
-  //   expect(find.text('0'), findsNothing);
-  //   expect(find.text('1'), findsOneWidget);
-  // });
-  ///Todo: implement tests.
-
-  testWidgets('Kanji detail page test', (WidgetTester tester) async {
-    await tester.pumpWidget(KanjiDetailPage(
-        kanji: Kanji(kanji: '一', grade: 1, jlpt: 1, strokes: 1, frequency: 1)));
-  });
-}
+void main() {}
